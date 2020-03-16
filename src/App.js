5a4ec2c7eaa14e0943e3db99eabeb6cdc2b49721
chikechris <cchike2017@gmail.com>
2020-03-16 16:22:04 -0400

    refactored code

--- src/App.js
+++ src/App.js
@@ -1,89 +1,120 @@
 import React, { useState } from 'react';
+import Form from './components/Form' 
+import Weather from './components/Weather'
 
 import './App.css';
 
-// const api = {
-//   key: "d8aed0cdcc48eed9be637fc927e2db93",
-//   url: "https://samples.openweathermap.org/data/2.5/"
-// }
+
+
+
 
 function App() {
-  const [query, setQuery] = useState('');
-  const [weatherData, setWeatherData] = useState({});
-  const search = et => {
-    if (et.key === 'Enter') {
-      fetch(
-        `http://api.weatherstack.com/current?access_key=2f9fb3f033b30a0bcddb462b5d945ee4&query=${query}`
-      )
-        .then(res => res.json())
-        .then(result => {
-          setWeatherData(result);
-          setQuery('');
-          console.log(result);
-        });
-    }
-  };
-  // const dateData = (d) => {
-  //   let months = ["January, February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  //   let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  // const [query, setQuery] = useState('');
+  // const [weatherData, setWeatherData] = useState({});
+  const [weather, setWeather] = useState([]); 
 
-  //   let day = days[d.getDay()]
-  //   let date = d.getDate()
-  //   let month = months[d.getMonth()]
-  //   let year = d.getFullYear()
-
-  //   return `${day} ${date} ${month} ${year}`
-  // }
-
-  function toFahrenheit(celsius) {
-    return (celsius * 9) / 5 + 32;
+  async function fetchData(e) {
+    e.preventDefault() 
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+   
+     const data =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=d8aed0cdcc48eed9be637fc927e2db93`)
+    .then(res => res.json())
+    .then(data => data)
+    setWeather({
+      temperature: Math.round(data.main.temp * 9/5 - 459.67),
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    }) 
   }
+
+  // const search = et => {
+  //   if (et.key === 'Enter') {
+  //     fetch(
+  //       `http://api.weatherstack.com/current?access_key=2f9fb3f033b30a0bcddb462b5d945ee4&query=${query}`
+  //     )
+  //       .then(res => res.json())
+  //       .then(result => {
+  //         setWeatherData(result);
+  //         setQuery('');
+  //         console.log(result);
+  //       });
+  //   }
+  // };
+ 
+  // function toFahrenheit(K) {
+
+  //   // =
+  //   return (K - 273.15) * 1.8000 + 32.00;
+  // }
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Weather App </h1>
       </header>
-      <div className='search-box'>
-        <input
-          type='text'
-          className='search-bar'
-          placeholder='Search City Name and click Enter.'
-          value={query}
-          onChange={e => setQuery(e.target.value)}
-          onKeyPress={search}
-        />
+      <Form  getWeather={fetchData}/> 
+      {console.log(weather)}
+      <Weather 
+        temperature={weather.temperature}
+        humidity={weather.humidity}
+        city={weather.city}
+        country={weather.country}
+        description={weather.description}
+        error={weather.error}
+      />
       </div>
-      {typeof weatherData.request != 'undefined' ? (
-        <>
-          <div className='location-box'>
-            <div className='location'>
-              {weatherData.location.name}, {weatherData.location.country}{' '}
-            </div>
-            <div className='date'>
-              Observed At: {weatherData.current.observation_time}{' '}
-            </div>
-            <div className='local-time'>
-              Local Date & Time: {weatherData.location.localtime}{' '}
-            </div>
-          </div>
-          <div className='weather-box'>
-            <div className='temp'>
-              {Math.round(toFahrenheit(weatherData.current.temperature))}°f
-            </div>
-            {/* <div className='icon'>
-              {<img src= {weatherData.current.weather_icons} />}
-            </div> */}
-            <div className='weather'>
-              {weatherData.current.weather_descriptions}
-            </div>
-          </div>
-        </>
-      ) : (
-        'please enter city name'
-      )}
-    </div>
   );
 }
+      export default App;
 
-export default App;
+
+
+//       {/* <div className='search-box'>
+//         {/* <input
+//           type='text'
+//           className='search-bar'
+//           placeholder='Search City Name and click Enter.'
+//           value={query}
+//           onChange={e => setQuery(e.target.value)}
+//           onKeyPress={search}
+//         /> */}
+//       {/* </div> */} 
+//       {/* // {typeof weatherData.request != 'undefined' ? ( */}
+// {/*         
+//       //   <>
+//       //     <div className='location-box'>
+//       //       <div className='location'>
+//       //         {weatherData.location.name}, {weatherData.location.country}{' '}
+//       //       </div>
+//       //       <div className='date'>
+//       //         Observed At: {weatherData.current.observation_time}{' '}
+//       //       </div>
+//       //       <div className='local-time'>
+//       //         Local Date & Time: {weatherData.location.localtime}{' '}
+//       //       </div>
+//       //     </div>
+//       //     <div className='weather-box'>
+//       //       <div className='temp'>
+//       //         {Math.round(toFahrenheit(weatherData.current.temperature))}°f
+//       //       </div>
+//       //       {/* <div className='icon'>
+//       //         {<img src= {weatherData.current.weather_icons} />}
+//       //       </div> */}
+//       {/* //       <div className='weather'>
+//       //         {weatherData.current.weather_descriptions}
+//       //       </div>
+//       //     </div> */}
+//       {/* //   </> */}
+
+//       {/* //  ) : (
+//       //   'please enter city name'
+//       // )}  
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Weather = ({ city, temperature, description, country, error }) => {
+  return (
+    <div>
+      {city && country && (
+        <p>
+          {city}, {''}
+          {country}
+        </p>
+      )}
+      {temperature && <p>Temperature:{temperature} °F</p>}
+      {description && <p>Description: {description}</p>}
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Weather;
