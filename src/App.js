278cf2b853b6ba9eb7bbbb99ed244c427dfc8c6a
chikechris <cchike2017@gmail.com>
2020-03-16 05:50:07 -0400

    pulling data from api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { useState } from 'react'
 
 import './App.css' 
 
-const api = {
-  key: "d8aed0cdcc48eed9be637fc927e2db93", 
-  url: "https://samples.openweathermap.org/data/2.5/"
+// const api = {
+//   key: "d8aed0cdcc48eed9be637fc927e2db93", 
+//   url: "https://samples.openweathermap.org/data/2.5/"
+// }
+
+function App () { 
+  const [query, setQuery] = useState('') 
+  const [weatherData, setWeatherData] = useState({
+
+  })
+const search = et => {
+  if (et.key === 'Enter') {
+    fetch(`http://api.weatherstack.com/current?access_key=2f9fb3f033b30a0bcddb462b5d945ee4&query=${query}`)
+    .then(res => res.json()) 
+    .then(result =>{ 
+      setWeatherData(result)
+    setQuery('')
+    console.log(result)
+    }
+    )
+  }
 }
-
-function App () {
-
 const dateData = (d) => {
   let months = ["January, February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
   let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] 
@@ -21,6 +36,8 @@ const dateData = (d) => {
   return `${day} ${date} ${month} ${year}`
 }
 
+
+
   return (
     <div className='App'>
       <header className='App-header'>
@@ -31,19 +48,26 @@ const dateData = (d) => {
         type="text"
           className= 'search-bar' 
           placeholder='Search....'
+          value={query}
+          onChange= {e =>setQuery(e.target.value)}
+          onKeyPress={search}
         />
       </div>
+      {(typeof  weatherData.request != 'undefined') ? (
+        <>
       <div className= 'location-box'>
-        <div className='location'>Atlanta, USA</div>
-          <div  className= 'date'>{dateData(new Date())} </div>   
+            <div className='location'>{weatherData.location.name}, {weatherData.location.country} </div>
+            <div className='date'>{weatherData.current.observation_time} </div>   
+            <div className='date'>{weatherData.location.localtime} </div>
       </div>
-      <div className='weather box'>
+      <div className='weather-box'>
           <div className='temp'>
             15*c
           </div> 
           <div className='weather'>Cold</div>
       </div>
-
+      </>
+            ) : ('')}
     </div>
   )
 }
