8b23c5dd0956d469d47db620663ffbb1b6f443e2
chikechris <cchike2017@gmail.com>
2020-03-16 04:40:58 -0400

    building out the weather infor content

--- src/App.js
+++ src/App.js
@@ -13,7 +13,12 @@ const dateData = (d) => {
   let months = ["January, February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
   let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] 
 
-  let day = day
+  let day = days[d.getDay()] 
+  let date = d.getDate() 
+  let month = months[d.getMonth()] 
+  let year = d.getFullYear()
+
+  return `${day} ${date} ${month} ${year}`
 }
 
   return (
@@ -31,6 +36,12 @@ const dateData = (d) => {
       <div className= 'location-box'>
         <div className='location'>Atlanta, USA</div>
           <div  className= 'date'>{dateData(new Date())} </div>   
+      </div>
+      <div className='weather box'>
+          <div className='temp'>
+            15*c
+          </div> 
+          <div className='weather'>Cold</div>
       </div>
 
     </div>
