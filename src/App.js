fc7a96f5f6165c554413b9693f4470da0df82ff9
chikechris <cchike2017@gmail.com>
2020-03-25 15:42:04 -0400

    added loading function

--- src/App.js
+++ src/App.js
@@ -5,73 +5,69 @@ import Weather from './components/weather/Weather';
 import './App.css';
 
 function App() {
-  const [query, setQuery] = useState("Atlanta")
+  const location = 'Atlanta, US';
+  const [query, setQuery] = useState('Atlanta'); 
+  const [loading, setLoading] = useState(false)
   const [weather, setWeather] = useState({
     city: null,
     temp: null,
     country: null,
     descrip: null,
-    humid: null,
-  })
-  
+    humid: null
+  });
 
-
-
-  const data = async (s) => {
+  // data fetching function
+  const getWeather = async s => {
+    setLoading(true)
     const apiRes = await fetch(
-      // `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=d8aed0cdcc48eed9be637fc927e2db93`
       `https://api.openweathermap.org/data/2.5/weather?q=${s}&APPID=d8aed0cdcc48eed9be637fc927e2db93`
     );
     const resJSON = await apiRes.json();
-    console.log(resJSON);
-    return resJSON;
+    setWeather({
+      temp: Math.round((resJSON.main.temp * 9) / 5 - 459.67),
+      humid: resJSON.main.humidity,
+      descrip: resJSON.weather[0].description,
+      country: resJSON.sys.country,
+      city: resJSON.name
+    });
+    setLoading(false)
   };
 
-const handleChange = (e) =>{
-  setQuery(e.target.value)
-}
- 
- const handleSearch = (e) => {
-    e.preventDefault() 
-      data(query).then(res => {
-        setWeather({
-          temp: (Math.round((res.main.temp * 9 / 5) - 459.67)),
-        humid: (res.main.humidity),
-        descrip: (res.weather[0].description),
-        country: (res.sys.country),
-        city: (res.name)
-        })    
-  });
- }
+  // function handles change form user end
+  const handleChange = e => {
+    setQuery(e.target.value);
+  };
+
+  // function handles search queries from user end
+  const handleSearch = e => {
+    e.preventDefault();
+    getWeather(query);
+  };
 
   useEffect(() => {
-    data(query).then(res => {
-      setWeather({
-        temp: (Math.round((res.main.temp * 9 / 5) - 459.67)),
-        humid: (res.main.humidity),
-        descrip: (res.weather[0].description),
-        country: (res.sys.country),
-        city: (res.name)
-      }) 
-    })
-  }, [weather])
-
+    getWeather(location);
+  }, [location]);
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Weather App </h1>
       </header>
-      <Form query={query} onChange={handleChange} onClick={handleSearch}/>
-      
-      <Weather
-        temp={weather.temp}
-        city={weather.city}
-        country={weather.country}
-        humid={weather.humid}
-        descrip = {weather.descrip}
-      
-      /> 
+  {!loading? (
+    <div> 
+        <Form query={query} onChange={handleChange} onClick={handleSearch} />
+
+        <Weather
+          temp={weather.temp}
+          city={weather.city}
+          country={weather.country}
+          humid={weather.humid}
+            descrip={weather.descrip} />
+  </div>
+  ):(
+    <div style={{color: "blue"}}>Loading</div>
+    )}
+     
     </div>
   );
 }
